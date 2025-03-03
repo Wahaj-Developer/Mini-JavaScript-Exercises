ec4c13bebb77cc4cc0477b1340a5356632175a81
Wahaj-Developer <ahmedwahaj317@gmail.com>
2025-03-03 15:18:46 +0500

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,60 @@
+//1  Print hellow word in three method
+/*
+ console.log("hellow world")
+ alert("hellow world")
+ document.write("hellow word")
+ */
+
+// //2 adding two num
+/*
+ let a = 5;
+ let b = 6;
+ var c = a+b;
+ console.log(c)
+*/
+
+// //3 adding two num from user side  just like a calculatoer
+/*
+ let a = prompt("please enter first number");
+ let b = prompt("please enter second number");
+ var c = parseInt(a)+parseInt(b);
+ console.log(c)
+*/
+
+// // 3 find the square root of a num 
+/*
+ var a = prompt("Enter a num")
+ var b = Math.sqrt(a)
+  console.log(b)
+*/
+
+//4  Find the area of a triangle (write triangle)
+/*
+var beas = prompt("please enter the base value");
+var height = prompt("please enter the height value");
+var area  = (beas* height)/2
+console.log(area)
+// 4  Find the area of a regular triangle (any type of triangle)
+/*
+var a = 10;
+var b = 5;
+var c = 10;
+var s  = (a+b+c)/2;
+var temp = s*(s-a)*(s-b)*(s-c);
+var area = Math.sqrt(temp)
+console.log(area)
+*/
+
+/*
+//4  Find the area of a regular triangle (any type of triangle)
+   value is get by user
+var a = parseFloat(prompt("Enter the value of side a:"));
+var b = parseFloat(prompt("Enter the value of side b:"));
+var c = parseFloat(prompt("Enter the value of side c:"));
+var s  = (a+b+c)/2;
+var temp = s*(s-a)*(s-b)*(s-c);
+var area = Math.sqrt(temp)
+console.log(area)
+*/
+
+//
