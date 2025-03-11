5b0e6ce182f444e7d6377b06be8dc6b0844cb210
Wahaj <ahmedwahaj317@gmail.com>
2025-03-12 01:30:08 +0500

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -317,3 +317,17 @@ console.log("The number of " + number + " factorial is = " +fact);
     console.log(i)
    }
  }
+
+// 15 Check the last digit is same or not
+// var a = prompt(("Please enter first number"));
+// var b = prompt(("Please enter second number"));
+// var c = prompt(("Please enter third number"));
+// var res1 = a%10;
+// var res2 = b%10;
+// var res3 = c%10;
+// if (res1 === res2 && res1 == res3){
+//   console.log ("Last number is equal.")
+// }
+// else {
+//   console.log("Last number is not equal.")
+// }
